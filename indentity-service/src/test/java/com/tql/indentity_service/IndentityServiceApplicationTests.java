9964ba5f1fb8bb92a9bd383948be22a44afb87d4
Your Name <you@example.com>
2024-07-29 15:06:14 +0700

    first init

---
+++ indentity-service/src/test/java/com/tql/indentity_service/IndentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tql.indentity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
